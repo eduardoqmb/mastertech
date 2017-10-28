a02b0d8c169eaa8776fd8d08c778361fbeccd8f1
eduardoqmb <33184370+eduardoqmb@users.noreply.github.com>
2017-10-28 17:08:00 -0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Insira seu código em javascript abaixo */
+
+console.log('Olá Mastertech!');
+
+$('.carousel').carousel({
+  interval: 2000
+})
+
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDP7LKIwecJ9tFWQUXxfluXP2xOSGKkK2Y&callback=initMap">
